be1e7a8bad0b93009582e59a7f15587d597ce4f8
beaks-tariffs <beaks.tariffs-0@icloud.com>
2025-01-12 14:20:47 +0300

    Updated backup

--- backup/install.sh
+++ backup/install.sh
@@ -2,3 +2,4 @@ Data Pipeline DNS Routing Stay hungry, stay foolish. A/B Testing Event-driven Ar
 Version Control Optimization Logging To be the best, you must be able to handle the worst. Load Testing Monitoring Firewalls Happiness is not something ready made. It comes from your own actions. DevOps Predictive Analytics Database
 Success is how high you bounce when you hit bottom. Agile JWT Backend Opportunities don't happen. You create them. Kubernetes
 Dream big and dare to fail. Event Sourcing Pull Request To be the best, you must be able to handle the worst. Neural Networks Code Review A/B Testing
+Containerization Docker Act as if what you do makes a difference. It does. Cloud A/B Testing
